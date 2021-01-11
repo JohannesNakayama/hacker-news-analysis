1db2a5f298eb8efaf42843242b06d45aa886bf0b
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2021-01-11 11:22:41 +0100

    refactored scraper

--- data_collection.jl
+++ data_collection.jl
@@ -1,19 +1,4 @@
-# libraries
-begin
-    using JSON
-    using HTTP
-    using DataFrames
-    using Dates
-    using Feather
-end
-
-# load scraper module
+using Feather
 include(joinpath("src", "HNScraper.jl"))
-
-# H1: There are only small changes over intervals of less than 5 minutes.
-h1_data = HNScraper.scrape_topstories(n_timeslices = 30, interval_minutes = 2)
-Feather.write(joinpath("data", "h1_data.feather"), h1_data)
-
-# test data over longer period for EDA
-eda_data = HNScraper.scrape_topstories(n_timeslices = 60, interval_minutes = 5)
-Feather.write(joinpath("data", "eda_data.feather"), eda_data)
+data = HNScraper.scrape_topstories(n_timeslices = 60 * 48, interval_minutes = 1)
+Feather.write(joinpath("data", "hacker_news_data.feather"), data)
--- src/functions.jl
+++ src/functions.jl
@@ -1,63 +1,53 @@
 # data structure for a story
 struct Story 
-    id::Int
+    id::Int64
     creator_username::String
     creation_time::Int64
     title::String
-    score::Int
-    comment_count::Int
+    score::Int64
+    comment_count::Int64
+    scrape_timestamp::Float64
 end
 
 # convenience constructor with named parameters
 Story(; 
-    id::Int, creator_username::String, creation_time::Int64, title::String, score::Int, comment_count::Int
+    id::Int64, creator_username::String, creation_time::Int64, title::String, score::Int64, comment_count::Int64, scrape_timestamp::Float64
 ) = Story(
-    id, creator_username, creation_time, title, score, comment_count
+    id, creator_username, creation_time, title, score, comment_count, scrape_timestamp
 )
 
 # default constructor for convenient filtering of faulty stories
-Story() = Story(id = -1, creator_username = "NA", creation_time = -1, title = "NA", score = -1, comment_count = -1)
+Story() = Story(id = -1, creator_username = "NA", creation_time = -1, title = "NA", score = -1, comment_count = -1, scrape_timestamp = -1)
 
 # data structure for a topstories list
 struct TopStoriesList 
     itemlist::Array{Story}
-    timestamp::Int
+    slice_timestamp::Float64
 end
 
 # get top stories from hacker news (every `interval_minutes` for a total of `n_timeslices` iterations)
 function scrape_topstories(; n_timeslices::Int, interval_minutes::Int)
-    if n_timeslices * interval_minutes > 600
-        @info "The configuration exceeds a running time of 10 hours. Aborting and returning `Nothing`."
-        return Nothing
-    end
-    if interval_minutes < 1
-        @info "Minimum interval is 1 minute. Setting interval = 1."
-        interval_minutes = 1
-    end
-
+    (interval_minutes >= 1) || throw(DomainError(interval_minutes, "sampling rate cannot be less than 1 minute"))
     df_list = []
     for i in 1:n_timeslices
         t = Timer(interval_minutes * 60)
         current_time = Dates.now()
-        print("\nRequest at $current_time", "\n")
+        @info "Request at $current_time"
         ts_df = log_topstories()
         push!(df_list, deepcopy(ts_df))
         @info "Logged time slice. Waiting for next request."
         wait(t)
     end
     topstories = reduce(vcat, df_list)
-
     return topstories
 end
                     
 # log topstories with timestamp of extraction time
 function log_topstories()
     topstories = get_topstories()
-
     topstories_df = DataFrames.DataFrame(topstories.itemlist)
-    topstories_df[!, :timestamp] .= topstories.timestamp
+    topstories_df[!, :slice_timestamp] .= topstories.slice_timestamp
     topstories_df[!, :rank_at_timestamp] = 1:length(topstories)
-
     return topstories_df
 end
 
@@ -65,39 +55,32 @@ Base.length(tsl::TopStoriesList) = length(tsl.itemlist)
 
 # get topstories 
 function get_topstories() 
-    timestamp = Int(floor(Dates.datetime2unix(Dates.now(UTC))))
-
     identifier = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    slice_timestamp = Dates.datetime2unix(Dates.now(UTC))
     response = HTTP.get(identifier)
     response_body = String(response.body)
     topstories_id_list = JSON.parse(response_body)
-
-    topstories = map(get_story, topstories_id_list)
-
-    return TopStoriesList(topstories, timestamp)
+    topstories = asyncmap(get_story, topstories_id_list, ntasks = 20)
+    return TopStoriesList(topstories, slice_timestamp)
 end
 
 # get story from id (factory-like)
 function get_story(story_id::Int)
     identifier = "https://hacker-news.firebaseio.com/v0/item/$story_id" * ".json" 
+    scrape_timestamp = Dates.datetime2unix(Dates.now(UTC))
     response = HTTP.get(identifier)
     response_body = String(response.body)
     story = JSON.parse(response_body)
-
     element_keys = keys(story)
-    id = "id" in element_keys ? story["id"] : -1
-    creator_username = "by" in element_keys ? story["by"] : "NA"
-    creation_time = "time" in element_keys ? story["time"] : -1
-    title = "title" in element_keys ? story["title"] : "NA"
-    score = "score" in element_keys ? story["score"] : -1
-    comment_count = "descendants" in element_keys ? story["descendants"] : -1
-
     return Story(
-        id = id,
-        creator_username = creator_username,
-        creation_time = creation_time,
-        title = title,
-        score = score,
-        comment_count = comment_count
-    )     
+        id = "id" in element_keys ? story["id"] : -1,
+        creator_username = "by" in element_keys ? story["by"] : "NA",
+        creation_time = "time" in element_keys ? story["time"] : -1,
+        title = "title" in element_keys ? story["title"] : "NA",
+        score = "score" in element_keys ? story["score"] : -1,
+        comment_count = "descendants" in element_keys ? story["descendants"] : -1,
+        scrape_timestamp = scrape_timestamp
+    )
 end
+
+# POTENTIAL TO DO: subscribe to updates
